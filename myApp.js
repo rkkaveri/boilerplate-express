9d164f4391c292ef04d652312eb21082cd593828
Kritika Kaveri <rkkaveri.kk@gmail.com>
2020-06-19 09:58:31 +0530

    first console added

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
-
+console.log("Hello World");
 
 // --> 11)  Mount the body-parser middleware  here
 
